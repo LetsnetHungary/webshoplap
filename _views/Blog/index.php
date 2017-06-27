a0aa5df59fe2ab8cbeb3417cffcf3e0c0453e226
kovacsoliver <kovacs.oliver@letsnet.hu>
2017-06-27 11:03:10 +0200

    blog

--- _views/Blog/index.php
+++ _views/Blog/index.php
@@ -1,1 +1,31 @@
-Blog body
+<?php include '../_views/_includes/mainheader.php'; ?>
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6">
+          <h1> <a href="link oldal">Title of the blog post</a></h1>
+          <h2>Írta: XY</h2>
+          <h3>Közzétéve: 2017.06.27.</h3>
+          <h3>További mini info</h3>
+        </div>
+
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+
+        </div>
+      </div>
+    </div>
