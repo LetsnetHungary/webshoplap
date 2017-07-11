db625c30c7d47ef5d813017bba2ec4e492f7e825
kovacsoliver <kovacs.oliver@letsnet.hu>
2017-07-11 16:04:44 +0200

    login

--- App/Controllers/Login_API.php
+++ App/Controllers/Login_API.php
@@ -1,8 +1,13 @@
 <?php
-	class Login extends CoreApp\Controller {
+	class Login_API extends CoreApp\Controller {
 
 		public function __construct() {
 			parent::__construct(__CLASS__);
       $this->loadModel(__CLASS__);
+			$this->userAuth();
+
+		}
+		public function userAuth(){
+			$this->model->logIn($_POST['email'], $_POST['password']);
 		}
   }
--- App/Models/Login_API_Model.php
+++ App/Models/Login_API_Model.php
@@ -5,4 +5,58 @@
 		{
 			parent::__construct();
 		}
+
+		public function logIn($email, $pw){
+			$db = CoreApp\DB::init(CoreApp\AppConfig::getData("database=>webshoplap"));
+			$stmt = $db->prepare("SELECT * FROM users WHERE users.email = :email ");
+			$stmt->execute(array(
+				":email" => $email
+			));
+			$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			// $pw_hash = password_hash($result[0]['password'], PASSWORD_BCRYPT); <- ezzel lehet a regisztráció passait hashelni
+			if (count($result) == 0) {
+				echo "Nem létezik ilyen felhasználó";
+			}
+			else { //létezik a felhasználó
+				if (password_verify($pw, $result[0]['password'])) { //megfelelő a jelszó
+					if ($result[0]['is_admin'] == 1) {
+						echo "Sikeres admin bejelentkezés!";
+						header("Location: Admin");
+					}
+					else {
+						echo "Sikeres felhasználó bejelentkezés";
+						header("Location: Profile");
+						return;
+					}
+				}
+				else { //jelszó nem megfelelő
+						echo "Hibás jelszó!";
+				}
+			} //1. else vége
+			// return $result;
+		} //logIn fun vége
+
+		public function sessionFun($email, $pw, $is_admin){
+			session_start();
+			$_SESSION['email'] = $email;
+			$_SESSION['password'] = $pw;
+			$_SESSION['is_admin'] = $is_admin; 
+		}
+
+		public function addUser(){
+			$db = CoreApp\DB::init(CoreApp\AppConfig::getData("database=>webshoplap"));
+			for ($i=1; $i < 10; $i++) {
+				$email = "valaki" . $i . "@test.com";
+				$pass = "pass" . $i;
+				$pass = password_hash($pass, PASSWORD_BCRYPT);
+				$stmt = $db->prepare("INSERT INTO `users`(`email`, `password`, `is_admin`) VALUES (:email, :password, :is_admin)");
+				$stmt->execute(array(
+					":email" => $email,
+					":password" => $pass,
+					":is_admin" => ($i%2)
+				));
+			}
+		}
+
+
 	}
