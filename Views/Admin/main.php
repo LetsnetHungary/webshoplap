5677a3561a7f6fe81041e8035752a9ed0726a61d
Máté Vásárhelyi <vasarhelyi.mate@gmail.com>
2017-07-11 17:35:04 +0200

    ibi

--- Views/Admin/main.php
+++ Views/Admin/main.php
@@ -2,6 +2,47 @@
 <body>
     <div class="container">
         <div class="row">
+            <div id="newshop" class="col-xs-12" style="display: none;">
+                <div class="box">
+                    <div class="boxTitle">
+                        <div class="boxTitleTitle">Új bolt hozzáadása</div>
+                    </div>
+                    <div class="boxContent" style="padding: 20px;">
+                        <div class="form-group">
+                            <label for="usr">Bolt neve:</label>
+                            <input type="text" class="form-control" id="shopname">
+                        </div>
+                        <div class="form-group">
+                            <label for="pwd">Webcím:</label>
+                            <input type="text" class="form-control" id="adress">
+                        </div>
+                        <div class="form-group">
+                            <label for="pwd">Telefon:</label>
+                            <input type="text" class="form-control" id="phone">
+                        </div>
+                        <div class="form-group">
+                            <label for="comment">Bio:</label>
+                            <textarea class="form-control" rows="5" id="bio"></textarea>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <ul id="labelholder" class="list-group">
+                                    <li class="list-group-item active">Címkék</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="newLabel">
+                                    <input type="text" class="form-control" id="labelname" placeholder="Új címke neve">
+                                    <button type="button" class="btn btn-success" id="addlabel">Hozzáadás</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="newLabel">
+                            <button type="button" class="btn btn-success" id="doneshop">Kész</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-xs-12 col-md-6 leftColumn" data-edit="false">
                 <div class="box">
                     <div class="boxTitle">
