891dd58af68046802d940f74d40199478c7539fe
kanyipi <kanyipicske@gmail.com>
2017-07-14 16:56:40 +0200

    jajj mamma én is dolgozok?: d lul

--- Views/includes/main/footer.php
+++ Views/includes/main/footer.php
@@ -1,4 +1,37 @@
-        <footer>
+     
+<footer class="container text-center bg-lightgray">
+<style>
+a:link {
+    color: black; 
+    background-color: transparent; 
+    text-decoration: none;
+}
+
+a:visited {
+    color: black;
+    background-color: transparent;
+    text-decoration: none;
+}
+
+a:hover {
+    color: black;
+    background-color: transparent;
+    text-decoration: none;
+}
+
+a:active {
+    color: black;
+    background-color: transparent;
+    text-decoration: none;
+}
+</style>
+<hr style="border-width: 4px 0;"
+>
+        <div style="padding-bottom: 20px">
+            ©webshoplap.hu&nbsp;&nbsp;&nbsp;&nbsp;<a href="Contact">Kapcsolat</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="assets/documents/mediaajanlat.pdf">Mediaajánlat</a>
+        </div>
+
+
         </footer>
     </body>
 </html>
