abb35641abd1398ae0ed624c2204307e031ab24d
Máté Vásárhelyi <vasarhelyi.mate@gmail.com>
2017-07-19 20:51:59 +0200

    as

--- Views/Blog/main.php
+++ Views/Blog/main.php
@@ -55,7 +55,7 @@ function humanTiming ($time)
                         <h5><span class="glyphicon glyphicon-time"></span> Közzétéve: <?php print_r($blog_post[$id]['blog_date']); ?></h5>
                         <h5><?php print_r($blog_post[$id]['blog_subtitle']); ?></h5>
                         <?php print_r($blog_post[$id][ 'blog_content']); ?>
-
+                        <div class="fb-share-button" data-href="http://www.webshoplap.graphed.hu/Blog?=<?php echo $_GET['post_id']; ?>" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Megosztás</a>
                     </div>
                     <div data-width="100%" class="fb-comments" data-href="<?php echo " http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI] "; ?>" data-numposts="5"></div>
                 </div>
@@ -105,7 +105,7 @@ function humanTiming ($time)
                         </div>
                         </div>
                         <? $count++; ?>
-                        <?php } }?>
+                        <?php } }?> 
                     </div>
                 </div>
                 <?php }else{
