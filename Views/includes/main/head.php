2d7147beb7292ff9f341d4fe581bdcd9a5d331d0
Máté Vásárhelyi <vasarhelyi.mate@gmail.com>
2017-07-20 17:25:13 +0200

    asd

--- Views/includes/main/head.php
+++ Views/includes/main/head.php
@@ -3,6 +3,11 @@
 	<head>
 		<link rel="stylesheet" href="/assets/css/main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta property="og:url"           content="http://www.your-domain.com/your-page.html" />
+		<meta property="og:type"          content="website" />
+		<meta property="og:title"         content="Your Website Title" />
+		<meta property="og:description"   content="Your description" />
+		<meta property="og:image"         content="http://www.your-domain.com/path/image.jpg" />
 	<? if($this->SEO) {?>
 		<meta charset = "<?php echo $this->SEO->seo->charset; ?>">
 		<title><?php echo $this->SEO->seo->title; ?></title>
