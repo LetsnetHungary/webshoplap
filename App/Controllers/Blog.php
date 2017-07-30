f307cfc25b6db991f2b9069050975a956f543398
hegelakos <hegel.akos@letsnet.hu>
2017-07-30 09:29:40 +0200

    Blog title fix

--- App/Controllers/Blog.php
+++ App/Controllers/Blog.php
@@ -6,7 +6,7 @@
 			$this->model = $this->loadModel(__CLASS__);
 			$this->viewInit("Blog", function() {
 					$this->view->blog_post = $this->model->blogLekeres();
-					$this->view->SEO->seo->title = isset($_GET['post_id']) ? $this->view->blog_post[$_GET['post_id']]['blog_title'] : "Blog";
+					$this->view->SEO->seo->title = isset($_GET['post_id']) ? $this->view->blog_post[$_GET['post_id'] -1 ]['blog_title'] : "Blog";
 			});
 
 		}
