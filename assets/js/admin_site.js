60b11085491185187054f328df301870f21222b5
Máté Vásárhelyi <vasarhelyi.mate@gmail.com>
2017-08-07 15:58:10 +0200

    dun

--- assets/js/admin_site.js
+++ assets/js/admin_site.js
@@ -420,10 +420,7 @@ function initShopSite() {
                                 <div class="pin-row">
                                     <i class="fa fa-thumb-tack fa-2x" aria-hidden="true"></i>
                                 </div>`+ t +`</li>`)
-                                console.log(element);
-                                console.log(pins);
-                if($.inArray(element, pins) != -1) {
-                    console.log('hsad');
+                if($.inArray(element+'', pins) != -1) {
                     a.addClass('pinned')
                 }
                 a.data('id',element).appendTo('#cat_holder')
