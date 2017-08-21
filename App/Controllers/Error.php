4959087eb7738404534ae5fe329d1df1a4bfcfda
hegelakos <hegel.akos@letsnet.hu>
2017-08-21 18:47:03 +0200

    most mar el leheterni az urlen

--- App/Controllers/Error.php
+++ App/Controllers/Error.php
@@ -7,7 +7,19 @@
         }
 
         public function checkCategoryRoute() {
+
             $category = explode("/", $_GET["url"])[0];
-            print_r($this->model->checkCategoryRoute($category));
+            
+            if($category == "Error") {
+                $this->viewInit("Error", function () use ($category) { });
+            }
+            else {
+                $this->viewInit("Category", function () use ($category) {
+                    $category = explode("/", $_GET["url"])[0];
+                    $this->view->shops = $this->model->checkCategoryRoute($category);
+                    $this->view->catname = $this->model->getCatName($category);
+                    $this->view->SEO->seo->title = $this->view->catname['name'];	
+                });
+            } 
         }
     }
