848085ce6296ef7c012b4922896f0c713a965760
hegelakos <hegel.akos@letsnet.hu>
2017-09-17 11:42:34 +0200

    shuffle bug

--- App/Models/Index_Model.php
+++ App/Models/Index_Model.php
@@ -33,14 +33,14 @@
       }
       public function getProducts() {
           $rem = 25;
-          $stmt = $this->db->prepare('SELECT * FROM `products` WHERE pinned=1 ORDER BY RAND() LIMIT 25');
+          $stmt = $this->db->prepare('SELECT * FROM `products` WHERE pinned=1 LIMIT 25');
           $stmt->execute(array());
           $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
           $rem = $rem - count($products);
           $stmt = $this->db->prepare('SELECT * FROM `products` WHERE pinned=0 ORDER BY RAND() LIMIT '.$rem );
           $stmt->execute(array());
           $products = array_merge($products,$stmt->fetchAll(PDO::FETCH_ASSOC));
-          shuffle($products);
+          //shuffle($products);
           return $products;
       }
       public function getPartners() {
