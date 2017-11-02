2bb897e4848c8490227c307cd79f39ff4e5903b8
akoshegel <hegel.akos@letsnet.hu>
2017-11-02 11:30:04 +0100

    Update main.php

--- Views/Blog/main.php
+++ Views/Blog/main.php
@@ -1,3 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-6861314656533682",
+    enable_page_level_ads: true
+  });
+</script>
 <?
 function humanTiming ($time)
 {
